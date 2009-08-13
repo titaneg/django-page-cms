f9f5bf6a407a5933656a1d478e10c6f1f9f4238f
Andrew Ingram <andy@local-administrators-mac-pro.local>
2009-08-13 10:17:54 +0100

    modify_content now updates page timestamps

--- pages/admin/views.py
+++ pages/admin/views.py
@@ -38,6 +38,7 @@ def modify_content(request, page_id, content_id, language_id):
             Content.objects.set_or_create_content(page, language_id,
                                                   content_id, content)
         page.invalidate()
+        page.save()
         return HttpResponse('ok')
     raise Http404
 modify_content = staff_member_required(modify_content)
